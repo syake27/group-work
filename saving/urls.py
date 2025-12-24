4b2603e8a82b00afe7f438eb732c3299636cc81b
syake27 <ayumu.inutsuka0427@gmail.com>
2025-12-24 10:47:29 +0900

    base.htmlのurl修正

--- saving/urls.py
+++ saving/urls.py
@@ -3,6 +3,9 @@ from . import views
 
 urlpatterns = [
     path("", views.home, name="home"),
+    path("home/", views.home, name="home"),
     path("base/", views.base, name="base"),
     path("saving-list/", views.saving_list, name="saving_list"),
+    path("ranking/", views.ranking, name="ranking"),
+    path("profile/", views.profile, name="profile"),
 ]
