eb497730875f88521769ba912511ded0523d658c
syake27 <ayumu.inutsuka0427@gmail.com>
2025-12-24 11:05:45 +0900

    新規登録、ログイン機能追加

--- saving/urls.py
+++ saving/urls.py
@@ -1,11 +1,18 @@
 from django.urls import path
 from . import views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path("", views.home, name="home"),
-    path("home/", views.home, name="home"),
     path("base/", views.base, name="base"),
     path("saving-list/", views.saving_list, name="saving_list"),
     path("ranking/", views.ranking, name="ranking"),
     path("profile/", views.profile, name="profile"),
+    path("signup/", views.signup, name="signup"),
+    path(
+        "login/",
+        auth_views.LoginView.as_view(template_name="saving/login.html"),
+        name="login",
+    ),
+    path("logout/", auth_views.LogoutView.as_view(), name="logout"),
 ]
